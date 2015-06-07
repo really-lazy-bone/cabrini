e7ae9921e1492881c18baf017da81b91392d39b1
Michael Hsu <pig0826usanj@gmail.com>
2015-06-07 08:47:32 -0700

    Create Tasks

--- cabrini-server/controllers/tasks.js
+++ cabrini-server/controllers/tasks.js
@@ -4,21 +4,12 @@ var Task = require('../models/Task');
 
 router.post('/create', function (req, res) {
   var TaskData = req.body;
+  var newTask = Task(TaskData);
+   newTask.save(function (err) {
+    if (err) throw err;
+    res.send(JSON.stringify(newTask));
+  });
  
  console.log(TaskData);
-  // var task = {
-  //     category: TaskData.category,  
-  //   };
- 
- 
- 
- 
-  // var newOrganization = Organization(organizationData);
-  // newOrganization.save(function (err) {
-  //   if (err) throw err;
-  //   delete newOrganization.password;
-  //   res.send(JSON.stringify(newOrganization));
-  // });
-
 });
 module.exports = router;
